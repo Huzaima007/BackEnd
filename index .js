a8ae7f3d28e6e6709936b9a2bce69440e799b360
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-15 13:35:57 +0500

    Update index .js

---
+++ index .js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+const data = [
+  {
+    name: "Huzaima",
+    email: "huzaima@gmail.com",
+  },
+  {
+    name: "Ahrar",
+    email: "ahrar@gmail.com",
+  },
+];
+
+app.get("/",(req,res)=>{
+    res.send(data);
+});
+
+app.listen  
